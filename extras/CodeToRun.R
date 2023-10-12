09ca32db3f8222099c7de470246905abf813b4d0
bdj5051 <43019260+bdj5051@users.noreply.github.com>
2023-10-12 19:31:30 +0900

    Update CodeToRun.R

--- extras/CodeToRun.R
+++ extras/CodeToRun.R
@@ -1,56 +1,33 @@
-
-.libPaths("C:/Users/BDJ/Documents/R/win-library/library_4")
 library(ODTP4HIRA)
-# Details for connecting to the server:
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "sql server",
-                                                                user = "djboo",
-                                                                password = "a1234",
-                                                                server = "10.19.10.248",
-                                                                pathToDriver = "C:/Users/BDJ/Documents")
-
-# The name of the database schema where the CDM data can be found:
-cdmDatabaseSchema <- "CDM_v531_YUHS.CDM"
-
-# The name of the database schema and table where the study-specific cohorts will be instantiated:
-cohortDatabaseSchema <- "COHORTDB.DJBOO"
-cohortTable <- "TEST1012"
-oracleTempSchema <- NULL
-
-# Some meta-information that will be used by the export function:
-databaseId <- "Synpuf"
-
-# The folder where the study intermediate and result files will be written:
-outputFolder <- "C:/Users/BDJ/Desktop/CDM/results/v8"
-connection <- DatabaseConnector::connect(connectionDetails)
-# 
-# # Optional: specify where the temporary files (used by the Andromeda package) will be created:
-# options(andromedaTempFolder = "s:/andromeda"))
-# 
-# # Maximum number of cores to be used:
+ 
+# Optional: specify where the temporary files (used by the Andromeda package) will be created:
+options(andromedaTempFolder = "s:/andromeda"))
+ 
+# Maximum number of cores to be used:
 maxCores <- parallel::detectCores()
-# 
-# # The folder where the study intermediate and result files will be written:
-# outputFolder <- "s:/ODTP4HIRA"
-# #
-# # # Details for connecting to the server:
-# connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
-#                                                                 server = Sys.getenv("PDW_SERVER"),
-#                                                                 user = NULL,
-#                                                                 password = NULL,
-#                                                                 port = Sys.getenv("PDW_PORT"))
-# 
-# # # The name of the database schema where the CDM data can be found:
-# cdmDatabaseSchema <- "CDM_IBM_MDCD_V1153.dbo"
-# # 
-# # # The name of the database schema and table where the study-specific cohorts will be instantiated:
-# cohortDatabaseSchema <- "scratch.dbo"
-# cohortTable <- "mschuemi_skeleton"
-# 
+ 
+# The folder where the study intermediate and result files will be written:
+ outputFolder <- "s:/ODTP4HIRA"
+
+# Details for connecting to the server:
+ connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
+                                                                 server = Sys.getenv("PDW_SERVER"),
+                                                                 user = NULL,
+                                                                 password = NULL,
+                                                                 port = Sys.getenv("PDW_PORT"))
+ 
+# The name of the database schema where the CDM data can be found:
+cdmDatabaseSchema <- "CDM_IBM_MDCD_V1153.dbo"
+ 
+# The name of the database schema and table where the study-specific cohorts will be instantiated:
+cohortDatabaseSchema <- "scratch.dbo"
+cohortTable <- "mschuemi_skeleton"
+
 # # Some meta-information that will be used by the export function:
-# databaseId <- "Synpuf"
+databaseId <- "HIRA"
 # 
 # # For Oracle: define a schema that can be used to emulate temp tables:
-# oracleTempSchema <- NULL
+oracleTempSchema <- NULL
 
 execute(connectionDetails = connectionDetails,
         cdmDatabaseSchema = cdmDatabaseSchema,
@@ -61,7 +38,7 @@ execute(connectionDetails = connectionDetails,
         databaseId = databaseId,
         databaseName = databaseId,
         databaseDescription = databaseId,
-        createCohorts = FALSE,
+        createCohorts = TRUE,
         synthesizePositiveControls = FALSE,
         runAnalyses = TRUE,
         packageResults = TRUE,
