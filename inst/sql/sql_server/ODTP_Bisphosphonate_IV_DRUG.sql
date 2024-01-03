dc7c1ac86466451b0ab5e88c528ae77b5a1fa625
bdj5051 <43019260+bdj5051@users.noreply.github.com>
2024-01-03 17:28:12 +0900

    Update drug cohort SQL files

--- inst/sql/sql_server/ODTP_Bisphosphonate_IV_DRUG.sql
+++ inst/sql/sql_server/ODTP_Bisphosphonate_IV_DRUG.sql
@@ -58,12 +58,12 @@ DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition
 INSERT INTO @target_database_schema.@target_cohort_table (cohort_definition_id, subject_id, drug_concept_id, cohort_start_date, cohort_end_date, days_supply, duration)
 select @target_cohort_id as cohort_definition_id, d.person_id, d.drug_concept_id, d.drug_exposure_start_date, d.drug_exposure_end_date, d.days_supply, d.duration
 from (
-  select de.*, 90 as 'duration'
+  select de.*, 90 as duration
   FROM @cdm_database_schema.DRUG_EXPOSURE de
   JOIN #Codesets codesets on ((de.drug_concept_id = codesets.concept_id and codesets.codeset_id = 1)) 
   WHERE days_supply >=1
 union all
-  select de.*, 365 as 'duration'
+  select de.*, 365 as duration
   FROM @cdm_database_schema.DRUG_EXPOSURE de
   JOIN #Codesets codesets on ((de.drug_concept_id = codesets.concept_id and codesets.codeset_id = 2)) 
   WHERE days_supply >= 1
--- inst/sql/sql_server/ODTP_Romosozumab_DRUG.sql
+++ inst/sql/sql_server/ODTP_Romosozumab_DRUG.sql
@@ -42,7 +42,7 @@ UNION  select c.concept_id
 -- Dmab
 DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition_id = @target_cohort_id;
 INSERT INTO @target_database_schema.@target_cohort_table (cohort_definition_id, subject_id, drug_concept_id, cohort_start_date, cohort_end_date, days_supply, duration)
-select @target_cohort_id as cohort_definition_id, d.person_id, d.drug_concept_id, d.drug_exposure_start_date, d.drug_exposure_end_date, d.days_supply, 30 as 'duration'
+select @target_cohort_id as cohort_definition_id, d.person_id, d.drug_concept_id, d.drug_exposure_start_date, d.drug_exposure_end_date, d.days_supply, 30 as duration
 FROM @cdm_database_schema.DRUG_EXPOSURE d
 JOIN #Codesets codesets on ((d.drug_concept_id = codesets.concept_id and codesets.codeset_id = 1))
 WHERE days_supply >=1;
