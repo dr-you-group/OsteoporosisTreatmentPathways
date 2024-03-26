3dadf6aafa911b959e408c9496822f58a76ef51c
bdj5051 <43019260+bdj5051@users.noreply.github.com>
2024-03-26 19:59:42 +0900

    Update [ODTP] SERM_DRUG.sql
    Update for HIRA-CDM

--- inst/sql/sql_server/[ODTP] SERM_DRUG.sql
+++ inst/sql/sql_server/[ODTP] SERM_DRUG.sql
@@ -1,52 +1,10 @@
-CREATE TABLE #Codesets (
-  codeset_id int NOT NULL,
-  concept_id bigint NOT NULL
-)
-;
-
-INSERT INTO #Codesets (codeset_id, concept_id)
-SELECT 0 as codeset_id, c.concept_id FROM (select distinct I.concept_id FROM
-( 
-  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (80502,4001645,40401864)
-UNION  select c.concept_id
-  from @vocabulary_database_schema.CONCEPT c
-  join @vocabulary_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
-  and ca.ancestor_concept_id in (80502)
-  and c.invalid_reason is null
-
-) I
-LEFT JOIN
-(
-  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (42538151,4002134,40480160,45766159)
-UNION  select c.concept_id
-  from @vocabulary_database_schema.CONCEPT c
-  join @vocabulary_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
-  and ca.ancestor_concept_id in (42538151,4002134,40480160,45766159)
-  and c.invalid_reason is null
-
-) E ON I.concept_id = E.concept_id
-WHERE E.concept_id is null
-) C UNION ALL 
-SELECT 1 as codeset_id, c.concept_id FROM (select distinct I.concept_id FROM
-( 
-  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in ((21602625,44814412,21602626,41181419))
-UNION  select c.concept_id
-  from @vocabulary_database_schema.CONCEPT c
-  join @vocabulary_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
-  and ca.ancestor_concept_id in ((21602625,44814412,21602626,41181419))
-  and c.invalid_reason is null
-
-) I
-) C
-;
 -- SERM
 DELETE FROM @target_database_schema.@target_cohort_table where cohort_definition_id = @target_cohort_id;
 INSERT INTO @target_database_schema.@target_cohort_table (cohort_definition_id, subject_id, drug_concept_id, cohort_start_date, cohort_end_date, days_supply, duration)
-select @target_cohort_id as cohort_definition_id, d.person_id, d.drug_concept_id, d.drug_exposure_start_date, d.drug_exposure_end_date, d.days_supply, 1 as duration
-FROM @cdm_database_schema.DRUG_EXPOSURE d 
-JOIN #Codesets codesets on ((d.drug_concept_id = codesets.concept_id and codesets.codeset_id = 1)) 
-WHERE days_supply >=28;
-
-
-TRUNCATE TABLE #Codesets;
-DROP TABLE #Codesets;
+select @target_cohort_id as cohort_definition_id, d.person_id, d.drug_concept_id, d.drug_exposure_start_date, d.drug_exposure_end_date, d.days_supply, d.duration
+from (
+  select de.*, 1 as 'duration'
+  FROM @cdm_database_schema.DRUG_EXPOSURE de
+  WHERE drug_concept_id in (41181419,21602626,44814412,21602625)
+	and days_supply >= 28
+  ) d;
