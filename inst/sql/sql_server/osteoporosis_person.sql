b1e9f7543bc41bf5bed9b8aa949e921de9a0cc50
bdj5051 <43019260+bdj5051@users.noreply.github.com>
2024-04-01 10:25:26 +0900

    Update osteoporosis_person.sql

--- inst/sql/sql_server/osteoporosis_person.sql
+++ inst/sql/sql_server/osteoporosis_person.sql
@@ -6,4 +6,4 @@ with osteoporosis as (
 	where co.condition_concept_id = o.concept_id and p.person_id = co.person_id and p.gender_concept_id = 8532
 	group by co.person_id, p.year_of_birth
 )
-select *, year(condition_start_date)-year_of_birth as age from person_osteoporosis;
+select *, year(condition_start_date)-year_of_birth as 'age' from person_osteoporosis;
